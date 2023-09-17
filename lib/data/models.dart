ae6cae9c1f5126bc63b59d74b0dd6dbbced1083e
aldyisza87 <57266292+aldyisza87@users.noreply.github.com>
2023-09-18 04:24:41 +0700

    create models data

---
+++ lib/data/models.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Product {
+  final String name;
+  final String price;
+  final String qty;
+  final String img;
+  final String dsc;
+  final String status;
+  final String rating;
+  Product(
+    this.name,
+    this.price,
+    this.qty,
+    this.img,
+    this.dsc,
+    this.rating,
+    this.status,
+  );
+}
+
+final List<Product> productDji = [
+  Product(
+      'DJI Mavic 3 Pro Combo (RC) - 4/3 CMOS Hasselblad',
+      '42.990.000',
+      '3',
+      'assets/mavic3.jpg',
+      'Sensor kamera 4/3 CMOS Hasselblad\nWaktu penerbangan hingga 43 menit\nJarak transmisi video hingga 15 km\nDilengkapi 8 sensor penglihatan dengan sudut lebar\nTeknologi Advanced Return to Home (RTH)',
+      '4.8',
+      'Tersedia'),
+  Product(
+      'DJI Air 3 (DJI RC-N2) - Camera Drone',
+      '17.500.000',
+      '0',
+      'assets/air3.jpg',
+      'Sensor wide angle 1/1,3-inci-CMOS \n Waktu penerbangan hingga 46 menit \n Jangkauan maksimal 20 km \n Kualitas video HDR 1080p/60fps \n Omnidirectional Obstacle Sensing \n Medium Tele & Wide-Angle Dual Primary Cameras',
+      '5.0',
+      'Terjual'),
+  Product(
+      'DJI Mavic 3 Pro Combo (RC) - 4/3 CMOS Hasselblad',
+      '42.990.000',
+      '3',
+      'assets/mavic3.jpg',
+      'Sensor kamera 4/3 CMOS Hasselblad\nWaktu penerbangan hingga 43 menit\nJarak transmisi video hingga 15 km\nDilengkapi 8 sensor penglihatan dengan sudut lebar\nTeknologi Advanced Return to Home (RTH)',
+      '4.8',
+      'Tersedia'),
+  Product(
+      'DJI Air 3 (DJI RC-N2) - Camera Drone',
+      '17.500.000',
+      '0',
+      'assets/air3.jpg',
+      'Sensor wide angle 1/1,3-inci-CMOS \n Waktu penerbangan hingga 46 menit \n Jangkauan maksimal 20 km \n Kualitas video HDR 1080p/60fps \n Omnidirectional Obstacle Sensing \n Medium Tele & Wide-Angle Dual Primary Cameras',
+      '5.0',
+      'Terjual'),
+  Product(
+      'DJI Mavic 3 Pro Combo (RC) - 4/3 CMOS Hasselblad',
+      '42.990.000',
+      '3',
+      'assets/mavic3.jpg',
+      'Sensor kamera 4/3 CMOS Hasselblad\nWaktu penerbangan hingga 43 menit\nJarak transmisi video hingga 15 km\nDilengkapi 8 sensor penglihatan dengan sudut lebar\nTeknologi Advanced Return to Home (RTH)',
+      '4.8',
+      'Tersedia'),
+  Product(
+      'DJI Air 3 (DJI RC-N2) - Camera Drone',
+      '17.500.000',
+      '0',
+      'assets/air3.jpg',
+      'Sensor wide angle 1/1,3-inci-CMOS \n Waktu penerbangan hingga 46 menit \n Jangkauan maksimal 20 km \n Kualitas video HDR 1080p/60fps \n Omnidirectional Obstacle Sensing \n Medium Tele & Wide-Angle Dual Primary Cameras',
+      '5.0',
+      'Terjual'),
+  Product(
+      'DJI Mavic 3 Pro Combo (RC) - 4/3 CMOS Hasselblad',
+      '42.990.000',
+      '3',
+      'assets/mavic3.jpg',
+      'Sensor kamera 4/3 CMOS Hasselblad\nWaktu penerbangan hingga 43 menit\nJarak transmisi video hingga 15 km\nDilengkapi 8 sensor penglihatan dengan sudut lebar\nTeknologi Advanced Return to Home (RTH)',
+      '4.8',
+      'Tersedia'),
+  Product(
+      'DJI Air 3 (DJI RC-N2) - Camera Drone',
+      '17.500.000',
+      '0',
+      'assets/air3.jpg',
+      'Sensor wide angle 1/1,3-inci-CMOS \n Waktu penerbangan hingga 46 menit \n Jangkauan maksimal 20 km \n Kualitas video HDR 1080p/60fps \n Omnidirectional Obstacle Sensing \n Medium Tele & Wide-Angle Dual Primary Cameras',
+      '5.0',
+      'Terjual'),
+  Product(
+      'DJI Mavic 3 Pro Combo (RC) - 4/3 CMOS Hasselblad',
+      '42.990.000',
+      '3',
+      'assets/mavic3.jpg',
+      'Sensor kamera 4/3 CMOS Hasselblad\nWaktu penerbangan hingga 43 menit\nJarak transmisi video hingga 15 km\nDilengkapi 8 sensor penglihatan dengan sudut lebar\nTeknologi Advanced Return to Home (RTH)',
+      '4.8',
+      'Tersedia'),
+  Product(
+      'DJI Air 3 (DJI RC-N2) - Camera Drone',
+      '17.500.000',
+      '0',
+      'assets/air3.jpg',
+      'Sensor wide angle 1/1,3-inci-CMOS \n Waktu penerbangan hingga 46 menit \n Jangkauan maksimal 20 km \n Kualitas video HDR 1080p/60fps \n Omnidirectional Obstacle Sensing \n Medium Tele & Wide-Angle Dual Primary Cameras',
+      '5.0',
+      'Terjual'),
+];
